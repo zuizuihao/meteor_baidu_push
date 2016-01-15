7d6fdc2c986615122c96b45fef238e57597d2195
Peng Huijian <peng.huijian@roadshr.com>
2016-01-15 17:08:04 +0800

    make files exist only server.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 reference to:
-http://www.push.baidu.com
+http://push.baidu.com/document
+
+fork from https://github.com/coderhaoxin/baidu-push.
 
 Use command to test your getui setting.
 meteor test-packages ./
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'zuizuihao:baidu-push',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Wrap baidu nodejs to meteor package.',
   // URL to the Git repository containing the source code for this package.
@@ -18,12 +18,12 @@ Npm.depends({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.addFiles('baidu.push.api.js', ['client', 'server']);
+  api.addFiles('baidu.push.api.js', ['server']);
 });
 
 Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
-  api.addFiles('baidu.push.api.js', ['client', 'server']);
-  api.addFiles('baidu-push-tests.js');
+  api.addFiles('baidu.push.api.js', ['server']);
+  api.addFiles('baidu-push-tests.js', ['server']);
 });
